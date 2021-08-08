b4a759d50e6822f18d972b39517f84eef1bb4c86
littlefire <aybwhite@gmail.com>
2021-08-08 12:05:50 -0700

    re initial commit

---
+++ checklist/src/components/todos/index.jsx
@@ -0,0 +1,14 @@
+import Todo from '../todo';
+import React, { useState } from 'react';
+
+//list of what needs to be done + delete button + checkbox
+const Todos = () => {
+    const todoState = useState(["do this"])
+    return  (
+    <div className="App">
+        <Todo/>
+    </div>
+    )
+}
+
+export default Todos;
---
+++ checklist/views/home.jsx
@@ -0,0 +1,13 @@
+
+import Todos from '../components/todos';
+import Todo from '../components/todo';
+
+//display
+const Home = () => (
+    <div className="App">
+        <Todos/>
+        <Todo/>
+    </div>
+)
+
+export default Home;
